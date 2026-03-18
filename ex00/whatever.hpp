95f4b08fd8312ea11afa33c9fca9d277cd57121c
Maja Kijewska <majkijew@student.42heilbornn.de>
2026-03-18 22:21:55 +0100

    init ex00, created files, makefile and copied the main from the subject

---
+++ ex00/whatever.hpp
@@ -0,0 +1,23 @@
+// • swap: Swaps the values of two given parameters. Does not return anything.
+// • min: Compares the two values passed as parameters and returns the smallest one.
+// If they are equal, it returns the second one.
+// • max: Compares the two values passed as parameters and returns the greatest one.
+// If they are equal, it returns the second one.
+
+#include <iostream>
+
+template <typename T> T swap(T x, T y) {
+	T temp;
+
+	temp = x;
+	x = y;
+	y = temp;
+}
+
+template <typename T> T min(T x, T y) {
+	return (x < y) ? x : y;
+}
+
+// template <typename T> T max() {
+
+// }
